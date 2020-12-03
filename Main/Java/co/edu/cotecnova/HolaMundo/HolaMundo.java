f231657d05b4297835a8e248c8dcf05d1d262713
Elkin Ramirez <eknramirez@gmail.com>
2020-12-02 20:59:38 -0500

    cambio de letrero a ojala

--- Main/Java/co/edu/cotecnova/HolaMundo/HolaMundo.java
+++ Main/Java/co/edu/cotecnova/HolaMundo/HolaMundo.java
@@ -3,7 +3,7 @@ package Main.Java.co.edu.cotecnova.HolaMundo;
 public class HolaMundo {
 
     public static void main(String[]args){
-        System.out.println("Hola mundo.");
+        System.out.println("Hola mundo que mas pues. 2020 ojala ya te acabes.");
 
     }
 }
