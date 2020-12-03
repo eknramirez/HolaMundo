a0c3c72fb40e4e118aeb8a258e704c53264fda73
Elkin Ramirez <eknramirez@gmail.com>
2020-12-02 20:53:48 -0500

    Actualizacion datos

---
+++ Main/Java/co/edu/cotecnova/HolaMundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package Main.Java.co.edu.cotecnova.HolaMundo;
+
+public class HolaMundo {
+
+    public static void main(String[]args){
+        System.out.println("Hola mundo.");
+
+    }
+}
---
+++ Test/Java/co/edu/cotecnova/HolaMundoTest.java
@@ -0,0 +1,10 @@
+package Test.Java.co.edu.cotecnova;
+
+public class HolaMundoTest {
+
+    public static void main(String[]args){
+        System.out.println("Hola mundo.");
+
+    }
+
+}
